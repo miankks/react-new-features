d2672b0d622a0e9b52d7e89d10bddbe6e42dbea1
Mian King <Bilal Jan>
2021-08-23 17:24:50 +0200

    useState and defaultProps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const App = (props) => {
+  // const array = useState(10)
+  const [count, setCount] = useState(props.count)
+
+  const increment = () => {
+    setCount(count +1);
+  }
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <button onClick={() => setCount(count -1)}>-1</button>
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count: 0
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App count={2}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
